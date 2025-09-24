15ba0d15c0ac8b06511b054b79df9d4b7028da17
Rizki <mhdrizky32@gmail.com>
2025-09-24 19:14:18 +0700

    Adding[feat]: Hero Section on Home Page with Background and CTA Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import Hero from "@/components/organisms/Hero";
 import React from "react";
 
 const page = () => {
   return (
-    <div className="max-w-screen-xl mx-auto py-6 px-4">
-      <h1 className="text-2xl">Hello Home Page</h1>
-    </div>
+    <>
+      <Hero />
+    </>
   );
 };
 
